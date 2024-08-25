a0123b529918e218bbc12c6468eb7847c75614d3
vqasimov12 <q.vaqif1211@gmail.com>
2024-08-25 20:04:26 +0400

    Task Manager

--- System_Programming_1/ProcessModel.cs
+++ System_Programming_1/ProcessModel.cs
@@ -7,7 +7,7 @@ public class ProcessModel
     public string Image { get; set; }
     public double Memory { get; set; }
     public double CpuUsage { get; set; }
-    public int ThreadCount { get; set; }
+    public int ThreadCount { get; set; } 
     public DateTime StartTime { get; set; }
     public int BasePriority { get; set; }
 }

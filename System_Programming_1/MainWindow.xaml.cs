ec10f13850dbb4fed062fd586d334397d601ae01
vqasimov12 <q.vaqif1211@gmail.com>
2024-08-27 17:42:35 +0400

    Task Manager

--- System_Programming_1/MainWindow.xaml.cs
+++ System_Programming_1/MainWindow.xaml.cs
@@ -1,21 +1,23 @@
-﻿using System.Collections.ObjectModel;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Timers;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace System_Programming_1;
 
 public partial class MainWindow : Window, INotifyPropertyChanged
 {
-    private List<ProcessModel> programs = [];
-    public List<ProcessModel> Programs { get => programs; set { programs = value; OnPropertyChanged(); } }
+    private string order = "";
+    private List<Process> programs = [];
+    public List<Process> Programs { get => programs; set { programs = value; OnPropertyChanged(); } }
     System.Timers.Timer Timer { get; set; }
     public MainWindow()
     {
         InitializeComponent();
         DataContext = this;
+        order = "Process name";
         Timer = new System.Timers.Timer(1000);
         Timer.Elapsed += GetPrograms;
         Timer.AutoReset = true;
@@ -23,66 +25,42 @@ public partial class MainWindow : Window, INotifyPropertyChanged
     }
     void GetPrograms(object sender, ElapsedEventArgs e)
     {
-        var programs = Process.GetProcesses().ToList();
-        foreach (var process in programs)
-        {
-            try
-            {
-                Programs.Add(new ProcessModel
-                {
-                    BasePriority = process.BasePriority,
-                    Id = process.Id,
-                    ProcessName = process.ProcessName,
-                    StartTime = process.StartTime,
-                    ThreadCount = process.Threads.Count,
-                    Image = process.MainModule?.FileName // No need for null-forgiving operator here
-                });
-            }
-            catch (Exception ex)
-            {
-                // Handle or log the exception as needed
-                // For instance, log the exception or ignore this process
-                Console.WriteLine($"Error processing {process.ProcessName}: {ex.Message}");
-            }
-        }
-
-
-
-        //MessageBox.Show(Programs[0]?.MainModule?.FileName);
-
-        //foreach (var process in allProcesses) 
-        //     Programs.Add(process);
-
-        //foreach (var process in allProcesses)
-        //{
-        //    try
-        //    {
-        //        var cpuCounter = new PerformanceCounter("Process", "% Processor Time", process.ProcessName, true);
-        //        cpuCounter.NextValue();
-        //        //Thread.Sleep(1000);
-        //        double cpuUsage = cpuCounter.NextValue() / Environment.ProcessorCount;
-        //        Programs.Add(new ProcessModel
-        //        {
-        //            Id = process.Id,
-        //            ProcessName = process.ProcessName,
-        //            Memory = process.WorkingSet64 / (1024 * 1024),
-        //            CpuUsage = cpuUsage,
-        //            ThreadCount = process.Threads.Count,
-        //            StartTime = process.StartTime,
-        //            BasePriority = process.BasePriority
-        //        });
-        //    }
-        //    catch
-        //    {
-        //        MessageBox.Show("Something went wrong please restart program", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-        //    }
-        //}
+        var p = Process.GetProcesses().ToList();
+        if (order == "Id")
+            Programs = p.OrderBy(z => z.Id).ToList();
+        else if (order == "Process name")
+            Programs = p.OrderBy(p => p.ProcessName).ToList();
+        else if (order == "Handles")
+            Programs = p.OrderBy(p => p.HandleCount).ToList();
+        else if (order == "Threads")
+            Programs = p.OrderBy(p => p.Threads.Count).ToList();
+        else if (order == "Memory")
+            Programs = p.OrderBy(p => p.PagedMemorySize).ToList();
     }
-
 
     public event PropertyChangedEventHandler? PropertyChanged;
     public void OnPropertyChanged([CallerMemberName] string? propertyName = null)
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private void BlackList_Button(object sender, RoutedEventArgs e)
+    {
+
+    }
+    private void Kill_Button(object sender, RoutedEventArgs e)
+    {
+        var button = sender as Button;
+        var process = button?.DataContext as Process;
+        process?.Kill();
+        GetPrograms(sender, null);
+    }
+
+    private void OrderButton_Click(object sender, RoutedEventArgs e)
+    {
+        var b = sender as Button;
+        if (b is null) return;
+        order = b.Content.ToString()!;
+        GetPrograms(sender, null!);
     }
 }
